97e71263677a66f794abc2e4e70436533ef4f7e5
Mac Parthum <macparthum@gmail.com>
2020-08-17 14:24:07 -0400

    heads and tails

---
+++ Assets/JS/javascript.js
@@ -0,0 +1,12 @@
+  
+var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://coin-flip1.p.rapidapi.com/headstails",
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-host": "coin-flip1.p.rapidapi.com",
+        "x-rapidapi-key": "5ae58be4c0mshda02cb58666e66dp1c926ejsnd57384ad94f9"
+    }
+}
+
